7ef2f87cfcb68cf4120abfe7dd56e4e111b03991
Wesley M. Conner <Wesley.M.Conner@gmail.com>
2024-07-05 09:18:18 -0400

    testBed3 Working AyncQ

--- testBed3.groovy
+++ testBed3.groovy
@@ -15,12 +15,22 @@
 // For reference:
 //   Unicode 2190 ← LEFTWARDS ARROW
 //   Unicode 2192 → RIGHTWARDS ARROW
-import com.hubitat.hub.domain.Event as Event
-import groovy.json.JsonOutput
-import groovy.json.JsonSlurper
 
 // The Groovy Linter generates NglParseError on Hubitat #include !!!
 #include WesMC.lUtils
+import com.hubitat.app.ChildDeviceWrapper as ChildDevW
+import com.hubitat.app.DeviceWrapper as DevW
+import com.hubitat.app.InstalledAppWrapper as InstAppW
+import com.hubitat.hub.domain.Event as Event
+import groovy.json.JsonOutput as JsonOutput
+import groovy.json.JsonSlurper as JsonSlurper
+import java.lang.Math as Math
+import java.lang.Object as Object
+
+import groovy.transform.Field
+import java.util.concurrent.SynchronousQueue
+
+@Field static SynchronousQueue<Map> q = [:]
 
 definition (
   name: 'TestBed3',
@@ -40,22 +50,44 @@ Map TestBed3() {
   return dynamicPage(
     name: 'TestBed3',
     title: 'TestBed3',
+    install: true,
     uninstall: true
   ) {
     app.updateLabel("TestBed3 (${app.id})")
     section{
-      // GIVEN A FIXED, ONE-LAYER MAP
-      Map w = [active: 'one', lifo: ['two', 'three', 'four']]
-      paragraph "${b('Given Map')}: ${w}"
-
-      // ENCODE AS JSON
-      String x = toJson(w)
-      paragraph "${b('Json Encoding')}: ${x}"
-
-      // RE-HYDRATE THE MAP IN ONE STEPS
-      //Map m = fromJson(x)
-      Map m = parseJson(x)
-      paragraph "${b('Hydration')}: ${m}"
+      paragraph 'thread test'
+      input( name: 'x',
+        title: "${b('Enter value for x')}",
+        type: 'text',
+        required: true
+      )
     }
   }
+}
+
+def consumer(Map parms = [ref: 'DEFAULT']) {
+  logInfo('consumer', "before take, ref: ${parms.ref}")
+  Map rxcmd = q.take()
+  logInfo('consumer', "after take, rxcmd: ${rxcmd}")
+}
+
+def producer(Map parms = [ref: 'DEFAULT']) {
+  logInfo('producer', "before put, ref: ${parms.ref}")
+  Map txcmd = [a: 'one', b: 'two', c: 'three']
+  q.put(txcmd)
+  logInfo('installed', 'after put')
+}
+
+void installed() {
+  logInfo('installed', 'before q creation')
+  q = new SynchronousQueue<Map>()
+  logInfo('installed', 'after q creation')
+  runInMillis(500, 'consumer', [data: [ref: "alpha"]])
+// ...
+
+  logInfo('installed', 'after consumer runIn 1')
+  runInMillis(1000, 'producer', [data: [ref: "alpha"]])
+  logInfo('installed', 'after producer runIn 1')
+  //pauseExecution(10000)
+  //logInfo('installed', 'after 10s pause')
 }
