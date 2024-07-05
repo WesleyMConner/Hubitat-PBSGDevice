a016ddac89f8b8367decfd0b5a1744cdb7a369d9
Wesley M. Conner <Wesley.M.Conner@gmail.com>
2024-07-05 14:46:30 -0400

    testBed3 WIP

--- testBed3.groovy
+++ testBed3.groovy
@@ -29,24 +29,10 @@ import java.lang.Object as Object
 
 import groovy.transform.Field
 import java.util.concurrent.SynchronousQueue
-import java.time.*
+import java.time.Instant
 import java.time.Duration
 
 @Field static SynchronousQueue<Map> q = [:]
-//@Field static SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-DD-HH:mm:ss.mmm")
-
-//import java.text.SimpleDateFormat
-//String newVersion() {
-//  //SimpleDateFormat df = new SimpleDateFormat("YYYY-MM-DD-HH:mm:ss.mmm")
-//  return sdf.format(new Date())
-//}
-
-//Long dateStringToDate(String s) {
-//  //SimpleDateFormat sdf = new SimpleDateFormat('YYYY-MM-DD-HH:mm:ss.mmm')
-//  return sdf.parse("YYYY-MM-DD-HH:mm:ss.mmm", cmd.ref)
-//}
-
-
 
 definition (
   name: 'TestBed3',
@@ -76,54 +62,77 @@ Map TestBed3() {
       Instant tIn1 = Instant.parse('2024-07-05T17:37:50.371291Z')
       Long durationMs1 = Duration.between(tIn1, tOut1).toMillis();
       paragraph "in: ${tIn1}, out: ${tOut1}, duration: ${durationMs1} ms"
+      paragraph "duration: ${durationMs1/1000} s"
 
       Instant tIn2 = java.time.Instant.now()
       pauseExecution(150)
       Instant tOut2 = java.time.Instant.now()
       Long durationMs2 = Duration.between(tIn2, tOut2).toMillis();
       paragraph "in: ${tIn2}, out: ${tOut2}, duration: ${durationMs2} ms"
+      paragraph "duration: ${durationMs2/1000} s"
 
-      //SimpleDateFormat sdf = new SimpleDateFormat("YYYYMMdd") //_HH:mm:ss.mmm")
-      //paragraph "sdf: ${sdf}"
-      //Date d = new Date()
-      //paragraph "d: ${d}"
-      //String ds = sdf.format(d)
-      //paragraph "ds: ${ds}"
-      //Date d2 = sdf.parse(ds)
-      //paragraph "d2: ${d2}"
-      paragraph h1('Thread Test')
-      input( name: 'x',
-        title: "${b('Enter value for x')}",
-        type: 'text',
-        required: true
-      )
+      paragraph h1('Click Done to begin Thread Test')
+      paragraph i('Review test output in Hubitat logs.')
     }
   }
 }
 
+/*
 def consumer(Map parms = [ref: 'DEFAULT']) {
   logInfo('consumer', "before take, ref: ${parms.ref}")
   Map rxcmd = q.take()
   logInfo('consumer', "after take, rxcmd: ${rxcmd}")
 }
+*/
 
+/*
 def producer(Map parms = [ref: 'DEFAULT']) {
   logInfo('producer', "before put, ref: ${parms.ref}")
   Map txcmd = [a: 'one', b: 'two', c: 'three']
   q.put(txcmd)
   logInfo('installed', 'after put')
 }
+*/
 
+void producer(Map parms) {
+  logInfo('producer', "parms: ${bMap(parms)}")
+  ArrayList log = ['']
+  // logInfo('producer',
+  //   "${parms.producer} has range: ${parms.range} (${getObjectClassName(parms.range)})"
+  // )
+  ArrayList cmds = parms.range.collect { e ->
+    [name: parms.name, value: "${e}", ref: "${java.time.Instant.now()}"]
+  }
+  logInfo('producer', "cmds: ${cmds}")
+  cmds.each{ command ->
+    pauseExecution(parms.pause)
+    q.put(command)
+    log << command
+  }
+  logInfo('producer', log)
+}
+
+void consumer(Map parms) {
+  logInfo('consumer', "parms: ${bMap(parms)}")
+  ArrayList log = ['']
+  ArrayList range = 1..30  // Tactically, limit looping to 75
+  range.each { e ->
+    logInfo('consumer', "At #${e}")
+    Map cmd = q.take()
+    Instant tOut = java.time.Instant.now()
+    Instant tIn = Instant.parse(cmd.ref)
+    Long qDuration = Duration.between(tIn, tOut).toMillis();
+    log << [ queued: qDuration, *:cmd ]
+  }
+  logInfo('consumer', log)
+}
 void installed() {
-  logInfo('installed', 'before q creation')
+  logInfo('installed', 'Creating queue ...')
   q = new SynchronousQueue<Map>()
-  logInfo('installed', 'after q creation')
-  runInMillis(500, 'consumer', [data: [ref: "alpha"]])
-// ...
-
-  logInfo('installed', 'after consumer runIn 1')
-  runInMillis(1000, 'producer', [data: [ref: "alpha"]])
-  logInfo('installed', 'after producer runIn 1')
-  //pauseExecution(10000)
-  //logInfo('installed', 'after 10s pause')
+  logInfo('installed', 'Queue created.')
+  runInMillis(1000, 'consumer', [data: [ref: "Single Consumer"]])
+  logInfo('installed', 'Consumer thread requested.')
+  Map args1 = [ producer: 'A', range: 1..30, name: 'alpha', pause: 150 ]
+  runInMillis(1000, 'producer', [data: args1])
+  logInfo('installed', 'Producer thread requested')
 }
